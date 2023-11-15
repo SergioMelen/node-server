5f5c2fa034dbe42edfccdfc00fbdec622d11013e
Sergio Melendez <sergiomelendez026@gmail.com>
2023-11-14 23:43:23 -0500

    node-promises

--- app.js
+++ app.js
@@ -1,82 +1,112 @@
 const readline = require('readline');
 
-let listaTareas = [];
-const mostrarTareas = () =>{
-    console.log(listaTareas);
+let listaTareas = [{
+    id: "1",
+    description: "lavar la ropa",
+    status: 'Pendiente'
+},
+{
+    id: "2",
+    description: "Limpiar la casa",
+    status: 'Pendiente' 
 }
-const agregarTarea = (id, description) => {
-    let newTask = {
-        id: id,
-        description: description,
-        isCompleted: false
-    };
-    listaTareas.push(newTask)
-    return newTask;
+];
+
+const mostrarTareas = () => {
+  console.log(listaTareas);
+  return Promise.resolve(listaTareas);
 };
 
-console.log(agregarTarea(1, "Entregar el proyecto"));
-console.log(agregarTarea(2, "Hacer oficio"));
+const agregarTarea = (description) => {
+  return new Promise((resolve) => {
+    const id = listaTareas.length > 0 ? (Number(listaTareas[listaTareas.length - 1].id) + 1).toString() : '1';
+    const newTask = {
+      id: id,
+      description: description,
+      status: 'Pendiente'
+    };
+    listaTareas.push(newTask);
+    console.log('Tarea agregada:', newTask);
+    resolve(newTask);
+  });
+};
 
 const eliminarTarea = (id) => {
-    listaTareas = listaTareas.filter(tarea => tarea.id !== id);
-    return listaTareas;
+  return new Promise((resolve) => {
+    const taskId = id.toString();
+    listaTareas = listaTareas.filter(tarea => tarea.id !== taskId);
+        resolve(listaTareas);
+  });
 };
 
 const completarTarea = (id) => {
-    let tareaCompletada = listaTareas.find(tarea => tarea.id == id);
-    if(tareaCompletada){
-      tareaCompletada.isCompleted = true;
-    } 
-    return listaTareas
+  return new Promise((resolve, reject) => {
+    const taskId = id.toString();
+  let tareaCompletada = listaTareas.find(tarea => tarea.id === taskId);
+    if (tareaCompletada) {
+      tareaCompletada.status = "Completada";
+      resolve(tareaCompletada);
+    } else {
+      console.log('No se encontró la tarea');
+      reject('Tarea no encontrada');
+    }
+  });
 };
 
-
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-})
+  input: process.stdin,
+  output: process.stdout,
+});
 
-rl.question('Elige un comando: (1)mostrar tareas , (2)añadir tarea, (3)completar tarea, (4)Eliminar tarea', (respuesta) => {
-     
-    console.log(`Haz elegido ${respuesta}`)
+const ejecutarComando = async () => {
+  rl.question('Elige un comando: (1)mostrar tareas, (2)añadir tarea, (3)completar tarea, (4)Eliminar tarea: ', async (respuesta) => {
+    console.log(`Haz elegido ${respuesta}`);
 
     switch (respuesta) {
-       case '1':
-           console.log(mostrarTareas())
-           break;
-           case '2':
-               rl.question('Ingrese el ID de la nueva tarea: ', (id) => {
-                   rl.question('Ingrese la descripción de la nueva tarea: ', (description) => {
-                         agregarTarea(id, description, isCompleted = false);
-                           console.log('Tarea agregada con éxito.', listaTareas);
-                           rl.close();
-                       
-                   });
-               });
-           break;
-       case '3':
-           rl.question('ingrese el id de la tarea que desea completar: ', (id)=> {
-            
-               completarTarea( id );
-               console.log('Tarea completada con éxito.');
-               mostrarTareas()
-               rl.close();
-           });
-           break;
-       case '4':
-           rl.question('ingrese el id de la tarea que desea eliminar: ', (id)=> {
-            const number = Number(id)
-               eliminarTarea( number );
-               console.log('Tarea elimanada con éxito.');
-               mostrarTareas();
-               rl.close();            
-          });
-          break;
-          default:
-           console.log('Comando no reconocido.');
-           rl.close();
-           break;
-   }});
+      case '1':
+        await mostrarTareas();
+        break;
+      case '2':
+        const descripcionTarea = await preguntarDescripcionTarea('Ingrese la descripción de la nueva tarea: ');
+        await agregarTarea(descripcionTarea);
+        console.log('Tarea agregada con éxito.');
+        break;
+      case '3':
+        const idCompletar = await preguntarDescripcionTarea('Ingrese el id de la tarea que desea completar: ');
+        try {
+          await completarTarea(Number(idCompletar));
+          console.log('Tarea completada con éxito.');
+        } catch (error) {
+          console.error('Error:', error);
+        }
+        await mostrarTareas();
+        break;
+      case '4':
+        const idEliminar = await preguntarDescripcionTarea('Ingrese el id de la tarea que desea eliminar: ');
+        await eliminarTarea(Number(idEliminar));
+        console.log('Tarea eliminada con éxito.');
+        await mostrarTareas();
+        break;
+      default:
+        console.log('Comando no reconocido.');
+        break;
+    }
 
+    const continuar = await preguntarDescripcionTarea('¿Quieres realizar otra operación? (s/n): ');
+    if (continuar.toLowerCase() === 's') {
+      ejecutarComando();
+    } else {
+      rl.close();
+    }
+  });
+};
 
+const preguntarDescripcionTarea = (pregunta) => {
+  return new Promise((resolve) => {
+    rl.question(pregunta, (respuesta) => {
+      resolve(respuesta);
+    });
+  });
+};
 
+ejecutarComando();
